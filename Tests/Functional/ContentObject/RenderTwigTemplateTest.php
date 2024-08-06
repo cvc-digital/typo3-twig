e7f881c5410a65db41c4d3e93496922ed3d95336
Lukas <netz@cvc.digital>
2024-08-06 15:10:41 +0200

    Update tests

--- Tests/Functional/ContentObject/RenderTwigTemplateTest.php
+++ Tests/Functional/ContentObject/RenderTwigTemplateTest.php
@@ -18,31 +18,38 @@
 
 namespace Cvc\Typo3\CvcTwig\Tests\Functional\ContentObject;
 
+use TYPO3\CMS\Core\Tests\Functional\SiteHandling\SiteBasedTestTrait;
 use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequest;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
 class RenderTwigTemplateTest extends FunctionalTestCase
 {
+    use SiteBasedTestTrait;
+
     protected array $testExtensionsToLoad = [
         'typo3conf/ext/cvc_twig',
         'typo3conf/ext/cvc_twig/Tests/Functional/Fixtures/Extensions/twig_test',
     ];
 
+    private const ROOT_PAGE_ID = 1;
+
     protected function setUp(): void
     {
-        // parent::setUp();
+        parent::setUp();
+        $this->importCSVDataSet(__DIR__.'/../../../.Build/vendor/typo3/cms-core/Tests/Functional/Fixtures/pages.csv');
+        $this->writeSiteConfiguration(
+            'frontend_twig_contentobject_test',
+            $this->buildSiteConfiguration(self::ROOT_PAGE_ID, '/'),
+        );
     }
 
     public function testTemplateIsRendered(): void
     {
-        $this->markTestSkipped('revisit later');
-
         $this->setUpFrontendRootPage(
-            1,
-            array(
-                'ntf://TypoScript/JsonRenderer.ts',
+            self::ROOT_PAGE_ID,
+            [
                 'EXT:cvc_twig/Tests/Functional/Fixtures/Extensions/twig_test/Configuration/TypoScript/page.typoscript'
-            )
+            ]
         );
         $response = $this->executeFrontendSubRequest((new InternalRequest())->withPageId(1));
 
