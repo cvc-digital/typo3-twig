1501c0c832464b7c7853ea859f7b9aa571a1b340
lukasnetz <54134036+lukasnetz@users.noreply.github.com>
2024-08-07 09:58:55 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.99.99',
+            'typo3' => '12.4.0-12.99.99',
         ],
     ],
 ];
