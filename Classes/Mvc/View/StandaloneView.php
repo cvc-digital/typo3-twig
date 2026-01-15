4653b3dcc94df211f02f45c0396ee34fdbc51f7b
Lukas <netz@cvc.digital>
2026-01-15 22:06:16 +0100

    feat: better logging of template paths

--- Classes/Mvc/View/StandaloneView.php
+++ Classes/Mvc/View/StandaloneView.php
@@ -22,7 +22,6 @@ use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
-use Twig\Loader\ChainLoader;
 use Twig\Loader\FilesystemLoader;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -54,7 +53,7 @@ final class StandaloneView
      *
      * @return string The rendered view
      */
-    public function render()
+    public function render(): string
     {
         $templatePaths = array_map([GeneralUtility::class, 'getFileAbsFileName'], $this->templateRootPaths);
 
@@ -68,15 +67,8 @@ final class StandaloneView
             $namespacedPaths = array_map([GeneralUtility::class, 'getFileAbsFileName'], $namespacedPaths);
             $fileSystemLoader->setPaths($namespacedPaths, $namespace);
         }
-
-        $originalLoader = $this->environment->getLoader();
-        $this->environment->setLoader(new ChainLoader([$fileSystemLoader, $originalLoader]));
-
-        $content = $this->environment->render($this->templateName, $this->variables);
-
-        $this->environment->setLoader($originalLoader);
-
-        return $content;
+        $this->environment->setLoader($fileSystemLoader);
+        return $this->environment->render($this->templateName, $this->variables);
     }
 
     /**
