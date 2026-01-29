61af8266a283b7b458bd77ed3d43f5cf733bede3
Lukas <netz@cvc.digital>
2026-01-29 01:15:08 +0100

    chore: update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,10 +24,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'opensource@cvc.digital',
     'author_company' => 'CARL von CHIARI GmbH',
     'state' => 'stable',
-    'version' => '3.0.0',
+    'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.99.99',
+            'typo3' => '13.4.0-14.99.99',
         ],
     ],
 ];
